08fe09f76a5fe4c660fc861587eaaec7cf33651e
Arjun-Sehgal <48253826+Sehgal-Arjun@users.noreply.github.com>
2023-01-11 14:04:29 +0000

    removing unnecessary console logs + bug fix

--- js/index-scripts.js
+++ js/index-scripts.js
@@ -4,24 +4,19 @@ function searching(){
     [...elements].forEach((element) => {
         element.style.display = 'block';
     });
-    
+
     if (userinput !== "" || userinput !== null){
         userinput = userinput.toLowerCase();
         userinput = userinput.replace(/ /g,"_");
         const elements = document.getElementById('artcontent').querySelectorAll('section');
-        console.log(elements);
         let list = [];
         let counter = 0;
         [...elements].forEach((element) => {
-            console.log(userinput + "-> what user typed");
-            console.log(element.getAttribute('artist') + "-> artist name per div");
             if (element.getAttribute('artist').includes(userinput)){
-                console.log('true');
                 list.push(counter);
             }     
             counter = counter + 1;   
         });
-        console.log(list);
         let indexlist = [];
         for (let i = 0; i < elements.length; i++){
             if (!list.includes(i)){
@@ -29,9 +24,7 @@ function searching(){
             }
         }
         //indexlist.reverse()
-        console.log(indexlist);
         for (let i = 0; i < indexlist.length; i++){
-            console.log(elements[indexlist[i]].getAttribute('artist'));
             elements[indexlist[i]].style.display = 'none';
         }
     }
